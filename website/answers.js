38003113b87a7da22452d4dfcd48322c5dbf58d7
Noah Naiman <nnaiman@bu.edu>
2017-10-25 00:00:25 -0400

    Can now get text from inputs. Need to parse it.

--- website/answers.js
+++ website/answers.js
@@ -2,14 +2,14 @@ $(function(){
     $("input").autoGrowInput({minWidth:0, comfortZone:0});
 });
 
-$('form').submit(function(){
-	$input = $('form :input');
-	console.log($input);
-});
-
-$('input').keypress(function(event){
-    if(event.which == 13){
-        event.preventDefault();
-        $("form").submit();
-    }
+$(document).keydown(function(e){
+	$(document).off("keydown");
+	$(document).unbind("keydown");
+	if(e.which == 13){
+		$('input[type="text"]').change(function(){
+		     var id = $(this).attr('id');
+		     console.log($('input[id="' + id + '"]').val());
+		     $('input[type="text"]').unbind();
+		});
+	}
 });
