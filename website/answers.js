013a968999d09f8fba003c6bf7d257544f18aea0
Noah Naiman <nnaiman@bu.edu>
2017-10-30 17:20:33 -0400

    Working on solivng function.

--- website/answers.js
+++ website/answers.js
@@ -7,23 +7,33 @@ $(document).keydown(function(e){
 		$('input[type="text"]').change(function(){
 		     var id = $(this).attr('id');
 		     var toParse = String($('input[id="' + id + '"]').val()).replace(' ', '').split('');
-		     var parsed = [];
-		     var cur = '';
-		     for(var i = 0; i < toParse.length; i++){
-		     	cur += toParse[i];
-		     	if(i+1 == toParse.length){
-		     		parsed.push(cur);
-		     	}
-		     	else if("=+-*/".includes(toParse[i+1])){
-		     		parsed.push(cur);
-		     		parsed.push(toParse[i+1]);
-		     		i++;
-		     		cur = '';
-		     	}
-		     }
-		     console.log(parsed);
+		     // console.log(toParse);
+		     // var parsed = [];
+		     // var cur = '';
+		     // for(var i = 0; i < toParse.length; i++){
+		     // 	cur += toParse[i];
+		     // 	if(i+1 == toParse.length){
+		     // 		parsed.push(cur);
+		     // 	}
+		     // 	else if("=+-*/".includes(toParse[i+1])){
+		     // 		parsed.push(cur);
+		     // 		parsed.push(toParse[i+1]);
+		     // 		i++;
+		     // 		cur = '';
+		     // 	}
+		     // }
+		     console.log(toParse);
 		     $('input[type="text"]').unbind();
 		     $("input").autoGrowInput({minWidth:0, comfortZone:0});
 		});
 	}
 });
+
+function solve(equation){
+	var restructured =[];
+	for(var i = 0; i < equation.length; i++) {
+		if(("1234567890".includes(equation[i])) && ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".includes(equation[i+2]) || "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".includes(equation[i-2]))){
+
+		}
+	}
+}
